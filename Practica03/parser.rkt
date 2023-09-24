40322512587ae63d14acfd069a2378c0dfb704b3
ulisessap <ulisessap12@ciencias.unam.mx>
2023-09-23 23:52:37 -0600

    Parser incompleto

--- Practica03/parser.rkt
+++ Practica03/parser.rkt
@@ -2,4 +2,19 @@
 
 (require "grammars.rkt")
 
-(define (parse s-exp))
+(define (parse s-exp)
+  (cond
+    [(number? s-exp) (num s-exp)]
+    [(symbol? s-exp) (id s-exp)]
+    [(boolean? s-exp) (bool a-exp)]
+    [(list? s-exp) (let ([head (car s-exp)])
+                     (case head
+                       [(+) (add (parse (second s-exp))
+                                 (parse (third s-exp)))]
+                       [(-) (sub (parse (second s-exp))
+                                 (parse (third s-exp)))]
+                       ;[(and) (op and (map parser (cdr s-exp)))]
+                       [(with) (let ([assign (second s-exp)])
+                                 (with (first assign)
+                                       (parser (second assign))
+                                       (parser (third s-exp))))]))]))
