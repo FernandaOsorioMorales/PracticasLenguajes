3078887677f9ef002e3157ba2be0087a9a8a5f17
SlemimanPzz <emilianoa@ciencias.unam.mx>
2023-09-28 23:23:38 -0600

    Interp

--- Practica03/interp.rkt
+++ Practica03/interp.rkt
@@ -8,7 +8,10 @@
     [id (i) (error 'interp "Variable libre.")]
     [num (n) n]
     [bool (b) b]
-    [op(f args) (foldr f (car args) (cdr args))]
+    ;[strinG (s) s]
+    [op(f args) (apply f (map (lambda (x) (interp x)) args))]
+    [with (bs bo) (interp (subst-list-bindings bs bo))]
+    [with* (bs bo) (interp (toWith (with* bs bo)))]
     [else (error 'interp"")]))
 
 (define (subst expr sub-id val)
@@ -16,34 +19,31 @@
     [id (i) (if (symbol=? i sub-id id) val expr)]
     [num (n) expr]
     [bool (b) (bool b)]
-    [op (f args) (op f (map (lambda (x) (subst sub-id val x)) args))]
-    [with (asings body) (subst-with asings body sub-id val)]
-    [with* (asings body) (subst-with2 asings body sub-id val)]
+    [op (f args) (op f (map (lambda (x) (subst x sub-id val)) args))]
+    [with (asings body) (subst-withAlt asings body sub-id val)]
+    [with* (asings body) (subst (toWith expr) sub-id val)]
     [else (error 'i "")]))
 
+(define (subst-withAlt ass body sub-id val)
+  (if (empty? (filter (lambda (x) (type-case Binding ass [binding (i valor) (symbol=? i sub-id)])) ass))
+      (with  (substBindings sub-id val ass) (subst body sub-id val))
+      (with (substBindings sub-id val ass) body)))
+ 
+(define (substBindings sub-id val bs)
+  (match  bs
+    ['() empty]
+    [(cons x xs) (cons (binding (binding-id x) (subst (binding-value x) sub-id val)) (substBindings sub-id val xs))]))
 
-;(define (subst-with assings body sub-id val)
-;  (let ([index-of-id (index-where assings (lambda (x) (type-case Binding assings [binding (i valor) (symbol=? i sub-id)]) assings))])
-;    (if (not index-of-id) (with (map (lambda (x) (type-case Binding assings [binding (i valor) (subst   )])) assings)
-;    (list-update assings index-of-id (lambda (x) (type-case Binding assings [binding (i valor)      ])))))))
-  
-
-
-(define (subst-with assings body sub-id val)
-  (if (empty? (filter (lambda (x) (type-case Binding assings [binding (i valor) (symbol=? i sub-id)])) assings))
-      (with (map (lambda (x) (type-case Binding assings [binding (i valor) (binding i (subst valor sub-id val))])) assings) (subst body sub-id val))
-      (with (map (lambda (x) (type-case Binding assings [binding (i valor) (binding i (subst valor sub-id val))])) assings) body)))
-
-
-(define (subst-with2 assings body sub-id val)
-  (if (empty? (filter (lambda (x) (type-case Binding assings [binding (i valor) (symbol=? i sub-id)])) assings))
-      (with* (map (lambda (x) (type-case Binding assings [binding (i valor) (binding i (subst valor sub-id val))])) assings) (subst body sub-id val))
-      (with* (map (lambda (x) (type-case Binding assings [binding (i valor) (binding i (subst valor sub-id val))])) assings) body)))
-  
-
-  
-  ;(let ([variable (findf (lambda (x) (type-case Binding assings [binding (i valor) (symbol=? i sub-id)]) assings))]) ()))
-  ;(if () ( with id (subst value sub-id val) body ) ( with id ( subst value sub-id val ) ( subst body sub-id val)))
+(define (subst-list-bindings bs bo)
+  (if (empty? bs) bo
+      [let ([head (car bs)])
+        (subst-list-bindings (cdr bs) (subst bo (binding-id head) (binding-value head)))]))
 
 
-
+(define (toWith w)
+  (type-case WAE with*
+     [with* (ass body)
+            (cond
+              [(empty? ass) body]
+              [else (with (list (car ass) (toWith (with* (cdr ass) body))))])]
+     [else (error "No es with*")]))
