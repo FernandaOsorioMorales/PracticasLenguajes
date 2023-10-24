7b8ef7e478747a94122aa429a43961b7c030f42d
SlemimanPzz <emilianoa@ciencias.unam.mx>
2023-10-23 22:23:13 -0600

    falta lo ultimo

--- Practica04/desugar.rkt
+++ Practica04/desugar.rkt
@@ -7,29 +7,32 @@
 (define (desugar expr)
   (type-case CFWSBAE expr
     [numS (n) (num n)]
-    [idS (i) (id i)]
+    [idS (x) (id x)]
     [boolS (b) (bool b)]
     [strinGS (s) (strinG s)]
     [opS (f args) (op f (map (lambda (a) (desugar a)) args))]
-    [funS (param bo) (fun param (desugar bo))]
-    [with*S (bs bo) (desugar (with*S-to-withS bs bo))]
+    [funS (param body) (fun param (desugar body))]
+    [with*S (bs bo) (desugar (toWiths bs bo))]
     [withS (bs bo)
           (let ([pairBS (bs-to-pair bs (cons '() '()))])
             (app (fun (reverse (car pairBS)) (desugar bo)) (reverse (cdr pairBS))))]
     [appS (f args) (app (desugar f) (map (lambda (a) (desugar a)) args))]
     [iFS (test then else) (iF (desugar test) (desugar then) (desugar else))]
-    [conDS (conds else) (desugar (conDS-to-iFS conds else))]))
+    [conDS (conds else) (desugar (condsToIfs conds else))]))
 
-(define (conDS-to-iFS conds else)
-  (if (empty? conds)
-      else
-      (type-case Condition (car conds)
-        [condition (test then) (iFS test then (conDS-to-iFS (cdr conds) else))])))
 
-(define (with*S-to-withS bs bo)
-  (cond
-    [(empty? bs) bo]
-    [else (withS (list (car bs)) (with*S-to-withS (cdr bs) bo))]))
+
+
+(define (condsToIfs conds else)
+  (match conds
+    ['() else]
+    [(cons x xs) (type-case Condition x
+        [condition (test then) (iFS test then (condsToIfs xs else))])]))
+
+(define (toWiths bs bo)
+  (match bs
+    ['() bo]
+    [(cons x xs) (withS (list x) (toWiths xs bo))]))
             
 (define (bs-to-pair bs acc)
   (if (empty? bs)
