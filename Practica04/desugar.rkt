cc20bea9c43ee8cd69851c57191b414ce6aa9325
SlemimanPzz <emilianoa@ciencias.unam.mx>
2023-10-23 20:03:10 -0600

    desugar y interp a medias mal

--- Practica04/desugar.rkt
+++ Practica04/desugar.rkt
@@ -4,4 +4,36 @@
 (require "parser.rkt")
 
 ;; desugar :: CFWSBAE -> CFSBAE
-(define (desugar expr))
+(define (desugar expr)
+  (type-case CFWSBAE expr
+    [numS (n) (num n)]
+    [idS (i) (id i)]
+    [boolS (b) (bool b)]
+    [strinGS (s) (strinG s)]
+    [opS (f args) (op f (map (lambda (a) (desugar a)) args))]
+    [funS (param bo) (fun param (desugar bo))]
+    [with*S (bs bo) (desugar (with*S-to-withS bs bo))]
+    [withS (bs bo)
+          (let ([pairBS (bs-to-pair bs (cons '() '()))])
+            (app (fun (reverse (car pairBS)) (desugar bo)) (reverse (cdr pairBS))))]
+    [appS (f args) (app (desugar f) (map (lambda (a) (desugar a)) args))]
+    [iFS (test then else) (iF (desugar test) (desugar then) (desugar else))]
+    [conDS (conds else) (desugar (conDS-to-iFS conds else))]))
+
+(define (conDS-to-iFS conds else)
+  (if (empty? conds)
+      else
+      (type-case Condition (car conds)
+        [condition (test then) (iFS test then (conDS-to-iFS (cdr conds) else))])))
+
+(define (with*S-to-withS bs bo)
+  (cond
+    [(empty? bs) bo]
+    [else (withS (list (car bs)) (with*S-to-withS (cdr bs) bo))]))
+            
+(define (bs-to-pair bs acc)
+  (if (empty? bs)
+      acc
+      (type-case Binding (car bs)
+        [binding (id value) (bs-to-pair (cdr bs) (cons (cons id (car acc))
+                                  (cons (desugar value) (cdr acc))))])))
--- Practica04/interp.rkt
+++ Practica04/interp.rkt
@@ -15,7 +15,55 @@
   [cons-env (id symbol?) (value CFSBAE-Val?) (rest-env Env?)])
 
 ;; interp :: CFSBAE x Env -> CFSBAE-Val
-(define (interp expr env))
+(define (interp expr env)
+    (match expr
+      [(num n) (num-v n)]
+      [(bool b) (bool-v b)]
+      [(strinG s) (string-v s)]
+      [(id x) (lookup x env)]
+      [(fun param body) (closure-v param body env)]
+      [(op f args) (apply f (map (lambda (x) (interp x env)) args))]
+      ;;[(app f args) (interp-app f args env)]
+      [(app fun-expr arg)
+       (let ([fun-val (interp fun-expr mt-env)])
+         (interp
+          (closure-v-body fun-val)
+          (cons-env (closure-v-args fun-val) (interp arg) (closure-v-env fun-val))))]
+      [(iF test then other) (interp-if (interp test env) then other env)]
+))
+
+
+(define (interp-app f args env)
+  (match f [(fun fargs body) (interp-app-aux fargs args body env)]))
+
+
+(define (interp-app-aux fargs args body env)
+  (if (= (length fargs) (length args))
+      (interp body (adds-to-env fargs args env))
+      (error 'interp "Numero de argumentos y parametros distinto")))
+
+
+(define (interp-if test then other env)
+  (match test
+    [(bool-v v) (if v (interp then env) (interp other env))]
+    [else (error 'interp "interp: La condición de una expresión if debe ser un booleano.")]))
+
 
 ;; lookup :: symbol x Env -> CFSBAE-Val
-(define (lookup sub-id env))
+(define (lookup sub-id env)
+  (match env
+    [mt-env (error 'interp (format "Variable libre ~a" sub-id))] 
+    [(cons-env id val renv) (if (symbol=? id sub-id) val (lookup sub-id renv))]
+  ))
+
+
+(define (adds-to-env fargs args env)
+  (if (= (length fargs) 0)
+      env
+      (adds-to-env (cdr fargs) (cdr args) (add-to-env (first fargs) (first args) env))))
+
+
+(define (add-to-env id val env)
+  (match env
+    [mtv-env (cons-env id val mtv-env)]
+    [(cons-env a b renv) (cons-env a b (add-to-env id val renv))]))
